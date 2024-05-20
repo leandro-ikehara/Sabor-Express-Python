6d42ccde49a4e5c850bb3da8e294dbc68cc9d33e
leandro-ikehara <leandro.ikehara@gmail.com>
2024-05-20 15:19:40 -0300

    Exercícios orientação a objetos

---
+++ musica-oo.py
@@ -0,0 +1,23 @@
+class Musica:
+    nome = ''
+    artista = ''
+    duracao = int
+
+musica1 = Musica()
+musica1.nome = 'Bohemian Rhapsody'
+musica1.artista = 'Queen'
+musica1.duracao = 355
+print(musica1.nome, musica1.artista, musica1.duracao)
+
+musica2 = Musica()
+musica2.nome = 'Imagine'
+musica2.artista = 'John Lennon'
+musica2.duracao = 183
+print(musica2.nome, musica2.artista, musica2.duracao)
+
+musica3 = Musica()
+musica3.nome = 'Shape of You'
+musica3.artista = 'Ed Sheeran'
+musica3.duracao = 234
+print(musica3.nome, musica3.artista, musica3.duracao)
+
