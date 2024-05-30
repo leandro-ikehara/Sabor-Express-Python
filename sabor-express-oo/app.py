723f74e36f31765440abaa8e4f661d8febebc679
leandro-ikehara <leandro.ikehara@gmail.com>
2024-05-30 19:00:02 -0300

    Limpeza de código e finalização do curso

--- sabor-express-oo/app.py
+++ sabor-express-oo/app.py
@@ -1,19 +1,5 @@
-import json
-
 import requests
-
-# from modelos.restaurante import Restaurante
-# from modelos.cardapio.bebida import Bebida
-# from modelos.cardapio.prato import Prato
-#
-#
-# restaurante_praca = Restaurante('praça', 'Gourmet')
-# bebida_suco = Bebida('Suco de Abacaxi', 5.0, 'grande')
-# bebida_suco.aplicar_desconto()
-# prato_paodequeijo = Prato('Pão de Queijo', 3.0, 'O melhor pão de queijo da cidade')
-# prato_paodequeijo.aplicar_desconto()
-# restaurante_praca.adicionar_no_cardapio(bebida_suco)
-# restaurante_praca.adicionar_no_cardapio(prato_paodequeijo)
+import json
 
 url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
 response = requests.get(url)
@@ -40,11 +26,3 @@ for nome_restaurante, dados in dados_restaurante.items():
     nome_arquivo = f'{nome_restaurante}.json'
     with open(nome_arquivo, 'w') as arquivo_restaurante:
         json.dump(dados, arquivo_restaurante, indent=4)
-
-def main():
-    pass
-    # restaurante_praca.exibir_cardapio
-
-
-if __name__ == '__main__':
-    main()
--- sabor-express-oo/main.py
+++ sabor-express-oo/main.py
@@ -7,13 +7,23 @@ app = FastAPI()
 def hello_world():
     '''
     Endpoint de abertura
+
     '''
     return {'Hello': 'World'}
+
+@app.get('/api/olamundo')
+def ola_mundo():
+    '''
+    Hello World, só que em Português
+    
+    '''
+    return {'Olá': 'Mundo'}
 
 @app.get('/api/restaurantes/')
 def get_restaurantes(restaurante: str = Query(None)):
     '''
     Endpoint de cardápio de restaurantes
+
     '''
     url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
     response = requests.get(url)
@@ -23,22 +33,14 @@ def get_restaurantes(restaurante: str = Query(None)):
         if restaurante is None:
             return {'Dados': dados_json}
 
-        # dados_restaurante = {}
         dados_restaurante = []
         for item in dados_json:
-            # nome_restaurante = item['Company']
             if item['Company'] == restaurante:
-            # if nome_restaurante not in dados_restaurante:
-            #     dados_restaurante[nome_restaurante] = []
-            #     dados_restaurante[nome_restaurante].append({
                 dados_restaurante.append({
                     "item": item['Item'],
-                    "preço": item['price'],
-                    "descrição": item['description']
+                    "price": item['price'],
+                    "description": item['description']
                 })
-        return {'Restaurante': restaurante, 'Cardapio':dados_restaurante}
-
-        # print(dados_restaurante['McDonald’s'])
+        return {'Restaurante': restaurante, 'Cardapio': dados_restaurante}
     else:
-        # print(f'O erro foi {response.status_code}')
         return {'Erro': f'{response.status_code} - {response.text}'}
